9ef5b9bc84d77e1cd24205d837bc9bf23ed08ab5
Devarshi Singh <tulsi.n.singh@gmail.com>
2026-06-01 04:08:34 +0530

    Checking github CI / CD

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello from FastAPI on Azure Functions!"}
-
+    return {"message": "Hello from FastAPI on Azure Functions! Version 2.0 via CI/CD!"}
 
 # This connects your FastAPI instance directly to the Azure Functions runtime handler
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
